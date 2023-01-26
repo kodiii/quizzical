3ccb18288a88390cf7cfa721a5295aa56fad101a
Ricardo Ribeiro <11535653+kodiii@users.noreply.github.com>
2023-01-26 20:35:29 +0100

    start

---
+++ src/components/Intro.jsx
@@ -0,0 +1,10 @@
+export default function Intro() {
+
+    return (
+        <div>
+            <h1>Quizzical</h1>
+            <p>Quizz game for lunatics</p>
+            <button>Start Quizz</button>
+        </div>
+    )
+}
