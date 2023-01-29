d666e51dfa4bd0a29ec400b9fa80057b6918474e
Ricardo Ribeiro <11535653+kodiii@users.noreply.github.com>
2023-01-29 18:22:13 +0100

    question implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-//import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Intro from './components/Intro'
-
+import Question from './components/Question'
 import './App.css'
 
 function App() {
+  const [quizz, setQuizz] = useState([])
 
+  useEffect(() => {
+    async function quizzApi() {
+      const res = await fetch(`https://opentdb.com/api.php?amount=5`)
+      const data = res.json()
+      setQuizz(data.results)
+    }
+    quizzApi()
+  }, [])
+
+  console.log(quizz)
+
+  const quizzQuestion = quizz.map(data => (
+      <Question 
+        key={data.key}
+        question={data.question}
+        correctAnswer={data.correct_answer}
+        incorrectAnswers={data.incorrect_answers}
+      />
+  ))
 
   return (
     <div className="App">
       <Intro />
+      {quizzQuestion}
     </div>
   )
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,10 @@
 export default function Intro() {
 
     return (
-        <div>
+        <div className="intro--container">
             <h1>Quizzical</h1>
-            <p>Quizz game for lunatics</p>
-            <button>Start Quizz</button>
+            <p className="p-intro">Quizz game for lunatics</p>
+            <button className="btn-start">Start Quizz</button>
         </div>
     )
 }
